6690dd4d7f3e3b735b8ef89fc28de66016166374
cjrcjrdl1 <cjrcjrdl1@naver.com>
2023-02-04 13:16:43 +0900

    일부 수정

--- login/src/main/java/hello/login/web/login/LoginController.java
+++ login/src/main/java/hello/login/web/login/LoginController.java
@@ -17,7 +17,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-import java.net.http.HttpResponse;
 
 @Slf4j
 @Controller
